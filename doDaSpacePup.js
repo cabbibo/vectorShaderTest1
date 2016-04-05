67f0e15ededde4a79c227850811187e792755d99
cabbibo <isaaclandoncohen@gmail.com>
2016-04-05 15:55:34 -0700

    amsd

---
+++ doDaSpacePup.js
@@ -0,0 +1,104 @@
+
+var G = {}
+
+G.loading = {
+
+      loaded:0,
+      neededToLoad:0
+
+    },
+
+G.shaders = new ShaderLoader( 'shaders' , 'shaders/chunks' );
+
+G.init = function(){
+
+  G.uniforms = {
+    time: {type:"f", value:0},
+    iModelMat:{type:"m4", value:new THREE.Matrix4()},
+    t_cube:{type:"t", value: null },
+    t_logo:{type:"t",value:G.logo},
+    noiseSize1:{type:"f", value:1},
+    noiseSize2:{type:"f", value:1},
+  }
+
+  G.uniforms.t_cube.value = G.skyMap;
+
+  var ar = window.innerWidth / window.innerHeight;
+
+  G.three = {
+
+    scene           : new THREE.Scene(),
+    camera          : new THREE.PerspectiveCamera( 40 , ar , .01 , 100 ),
+    renderer        : new THREE.WebGLRenderer(),
+    clock           : new THREE.Clock(),
+    stats           : new Stats()
+
+  }
+
+  G.three.renderer.setSize( window.innerWidth, window.innerHeight );
+  G.three.renderer.setClearColor( 0x000000 , 1 )
+  G.three.renderer.domElement.id = "renderer"
+  G.three.renderer.setPixelRatio(  2 );
+  document.body.appendChild( G.three.renderer.domElement );
+
+  G.three.stats.domElement.style.position = "absolute";
+  G.three.stats.domElement.style.left = "0px";
+  G.three.stats.domElement.style.bottom = "-30px";
+  G.three.stats.domElement.style.zIndex = "999";
+ // document.body.appendChild( G.three.stats.domElement );
+
+
+  G.controls = new THREE.TrackballControls( G.three.camera );
+  G.controls.noZoom = true;
+  G.controls.noPan = true;
+  G.controls.noRoll = true;
+  G.three.camera.position.z = .3;
+
+  G.doDaSpacePup();
+
+  
+}
+
+G.animate = function(){
+
+  requestAnimationFrame( G.animate );
+  G.controls.update();
+
+  G.uniforms.time.value += G.three.clock.getDelta();
+  G.sp.rotation.y = .3 * Math.sin( G.uniforms.time.value * 1.);
+  G.sp.rotation.x = .2 * Math.sin( G.uniforms.time.value * 1.3);
+
+  //G.sp.position.x = Math.sin( G.uniforms.time.value * .1 ) * .01;
+  G.uniforms.iModelMat.value.getInverse( G.sp.matrixWorld );
+
+  G.three.renderer.render( G.three.scene , G.three.camera );
+  G.three.stats.update();
+
+  
+}
+
+G.doDaSpacePup = function(){
+
+  var mat = new THREE.ShaderMaterial({
+    uniforms: G.uniforms,
+    vertexShader:G.shaders.vs.trace,
+    fragmentShader:G.shaders.fs.trace,
+  });
+
+  //var mat = new THREE.MeshNormalMaterial({
+  //  side: THREE.DoubleSide
+  //});
+  
+
+  var geo = new THREE.CylinderGeometry( 0,.1,.13,3,1);
+  var geo = new THREE.PlaneGeometry(.15,.15 );
+
+  var mesh = new THREE.Mesh( geo , mat );
+  //mesh.rotation.
+  
+
+  G.sp = mesh;
+  G.three.scene.add( mesh );
+
+}
+
---
+++ loadFunctions.js
@@ -0,0 +1,186 @@
+function beginLoad(){
+  loadShaders();
+  loadAudio();
+  G.logo = loadImage("starFace.jpg");
+  loadCubeMap();
+}
+
+function loadShaders(){
+
+  //shaders.load( 'ss-curlFront'    , 'sim'    , 'simulation' );
+
+  G.loading.neededToLoad ++;
+
+  G.shaders.load( 'fs-trace'  , 'trace' , 'fragment' );
+  G.shaders.load( 'vs-trace'  , 'trace' , 'vertex'   );
+
+  
+  G.shaders.shaderSetLoaded = function(){
+    onLoad();
+  }
+
+}
+
+
+
+function loadImage(url){
+
+  G.loading.neededToLoad ++;
+  var r = THREE.RepeatWrapping;
+
+  var t = THREE.ImageUtils.loadTexture(url, r , onLoad, onError);
+  t.wrapT = t.wrapS = THREE.RepeatWrapping;
+  return t;
+
+}
+
+function loadCubeMap(){
+
+  G.loading.neededToLoad ++;
+  G.loading.neededToLoad ++;
+  G.loading.neededToLoad ++;
+  G.loading.neededToLoad ++;
+
+  var path = "milky/dark-s_";
+  var format = '.jpg';
+  var urls = [
+      path + 'px' + format, path + 'nx' + format,
+      path + 'py' + format, path + 'ny' + format,
+      path + 'pz' + format, path + 'nz' + format
+    ];
+
+
+
+
+
+
+
+  var loader = new THREE.CubeTextureLoader();
+  //loader.onLoad = 
+
+
+
+
+  var path = "skybox/";
+  var format = '.jpg';
+  var urls = [
+      path + 'px' + format, path + 'nx' + format,
+      path + 'py' + format, path + 'ny' + format,
+      path + 'pz' + format, path + 'nz' + format
+    ];
+  var reflectionCube = loader.load( urls ,function(){
+    console.log("HAHA");
+    onLoad();
+  });
+  reflectionCube.format = THREE.RGBFormat;
+  //G.cubeMap = reflectionCube;
+  G.skyMap = reflectionCube;
+
+
+
+
+  var path = "park/";
+  var format = '.jpg';
+  var urls = [
+      path + 'posx' + format, path + 'negx' + format,
+      path + 'posy' + format, path + 'negy' + format,
+      path + 'posz' + format, path + 'negz' + format
+    ];
+
+
+  var parkCube = loader.load( urls ,function(){
+    console.log("HAHA");
+    onLoad();
+  });
+  parkCube.format = THREE.RGBFormat;
+  G.parkMap = parkCube;
+
+
+  var path = "castle/";
+  var format = '.jpg';
+  var urls = [
+      path + 'px' + format, path + 'nx' + format,
+      path + 'py' + format, path + 'ny' + format,
+      path + 'pz' + format, path + 'nz' + format
+    ];
+
+
+  var castleCube = loader.load( urls ,function(){
+    console.log("HAHA");
+    onLoad();
+  });
+  castleCube.format = THREE.RGBFormat;
+  G.castleMap = castleCube;
+
+
+  var path = "pisa/";
+  var format = '.png';
+  var urls = [
+      path + 'px' + format, path + 'nx' + format,
+      path + 'py' + format, path + 'ny' + format,
+      path + 'pz' + format, path + 'nz' + format
+    ];
+
+
+  var pisaCube = loader.load( urls ,function(){
+    console.log("HAHA");
+    onLoad();
+  });
+  pisaCube.format = THREE.RGBFormat;
+  G.pisaMap = pisaCube;
+}
+
+
+function loadAudio(){
+
+  //loadBuffer( "loveLoopBuffer"  , "audio/love.mp3"      );
+  //loadBuffer( "painLoopBuffer"  , "audio/pain.mp3"      );
+  //loadBuffer( "normLoopBuffer"  , "audio/norm.mp3"      );
+//
+  //loadBuffer( "clickNoteBuffer" , "audio/switch.mp3"    );
+  //loadBuffer( "startNoteBuffer" , "audio/startNote.mp3" );
+  //loadBuffer( "jestNoteBuffer"  , "audio/jest.mp3" );
+
+
+
+}
+
+function loadBuffer(name , bufferFile){
+
+  var aBuff = new AudioBuffer( G.audio , bufferFile);
+  G[name] = aBuff;
+  G.loading.neededToLoad += 1;
+  aBuff.addLoadEvent(function(){
+    onLoad();
+  })
+
+}
+
+function onLoad(){
+
+  G.loading.loaded ++;
+
+  console.log( G.loading );
+
+
+  if( G.loading.loaded == G.loading.neededToLoad ){
+
+    finishedLoading();
+
+  }
+
+}
+
+// TODO: these catch?
+function onProgress(e){
+  console.log( e );
+}
+
+function onError(e){
+  console.log( e );
+}
+
+function finishedLoading(){
+  G.init(); 
+  G.animate();
+}
